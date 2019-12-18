8dc1b85da94a5b967a32dcbd83a44ec0b9307abf
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-12-18 22:23:51 +0700

    add - layout for blog

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,68 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import _ from "lodash"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+    <a className="navbar-brand" href="#">
+      {siteTitle}
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#collapsibleNavbar"
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="blog/page/1" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Categories
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <StaticQuery
+              query={graphql`
+                query CategoryQuery {
+                  allMarkdownRemark(limit: 2000) {
+                    group(field: frontmatter___tags) {
+                      fieldValue
+                    }
+                  }
+                }
+              `}
+              render={data => {
+                return data.allMarkdownRemark.group.map(g => {
+                  return (
+                    <Link to={`tags/${g.fieldValue}`} className="dropdown-item">
+                      {_.capitalize(g.fieldValue)}
+                    </Link>
+                  )
+                })
+              }}
+            />
+          </div>
+        </li>
+      </ul>
     </div>
-  </header>
+  </nav>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,11 +4,10 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import { Helmet } from "react-helmet"
 import Header from "./header"
 import "./layout.css"
 
@@ -22,9 +21,32 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
   return (
     <>
+      <Helmet>
+        <title>Gatsby Blog</title>
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossOrigin="anonymous"
+        ></script>
+        <link
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
